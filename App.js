bb94312dab9c8fb05ee48d8e2970ff7ebd58e902
Bruno Bispo <brunobispoo25@gmail.com>
2021-05-24 21:02:09 -0300

    First Commit

---
+++ App.js
@@ -0,0 +1,19 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import Home from './views/Home'
+
+export default function App() {
+
+  const Stack = createStackNavigator();
+
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={Home} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
